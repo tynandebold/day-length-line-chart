afa4f6bd6dd9eba4a97f99f25aab06d14d488256
Tynan DeBold <tynan.debold@tradeshift.com>
2021-05-23 15:53:06 +0200

    update data

--- data/index.js
+++ data/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const { locations } = require('./locations');
 
 const today = new Date().toISOString().slice(0, 10);
+// const forgottenDate = '2021-05-23';
 
 function writeData() {
   let dataPoints = [];
@@ -16,6 +17,7 @@ function writeData() {
         `https://api.sunrise-sunset.org/json?lat=${coords[0]}&lng=${coords[1]}`
       );
       const dayLength = response.data.results.day_length;
+      console.log(dayLength);
       const dayLengthSecs = computeDayLengthSeconds(dayLength);
 
       dataPoints.push(`${today},"${location}",${dayLength},${dayLengthSecs}`);
@@ -33,6 +35,8 @@ function writeData() {
 }
 
 function computeDayLengthSeconds(dayLength) {
+  if (dayLength === '00:00:00') return 86400;
+
   const lengthParts = dayLength.split(':');
   return +lengthParts[0] * 60 * 60 + +lengthParts[1] * 60 + +lengthParts[2];
 }
